5cb44bafad74907f1a5c748750552d20d333b9d2
73pi <18852855260@163.com>
2019-02-19 16:20:52 +0800

    添加debug

---
+++ properties/main.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-#
+
+#-------------------------------------------------------------------------------
+# Name:         main
+# Description:  
+# Author:       73
+# Date:         2019/2/19
+#-------------------------------------------------------------------------------
+
+from scrapy.cmdline import execute
+import sys
+import os
+
+sys.path.append(os.path.dirname(os.path.abspath(__file__)))
+execute(["scrapy", "crawl", "basic"])
+# debug运行main
